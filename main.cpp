edc43fc9d9a448ff88f13d84f2c890549ee95413
Md Ashik Pk <ashik26518@gmail.com>
2025-09-13 04:54:20 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include<string.h>
-
+#include <string.h>
 
 struct Patient {
     char name[100];
     int id;
+    int age;
+    char gender[10];
     char address[200];
     char mobile[20];
     char doctor[100];
+    char reference[200];
+    float totalAmount;
+    float discount;
+    float dueAmount;
 };
 
 int main() {
@@ -15,13 +20,19 @@ int main() {
 
     printf("------ Hospital Management System ------\n");
 
-
     printf("Enter Patient Name: ");
     fgets(p.name, sizeof(p.name), stdin);
 
     printf("Enter Patient ID: ");
     scanf("%d", &p.id);
     getchar();
+
+    printf("Enter Patient Age: ");
+    scanf("%d", &p.age);
+    getchar();
+
+    printf("Enter Patient Gender: ");
+    fgets(p.gender, sizeof(p.gender), stdin);
 
     printf("Enter Patient Address: ");
     fgets(p.address, sizeof(p.address), stdin);
@@ -32,15 +43,32 @@ int main() {
     printf("Enter Doctor Name: ");
     fgets(p.doctor, sizeof(p.doctor), stdin);
 
+    printf("Enter Reference Analysis: ");
+    fgets(p.reference, sizeof(p.reference), stdin);
+
+    printf("Enter Total Amount: ");
+    scanf("%f", &p.totalAmount);
+
+    printf("Enter Discount Amount: ");
+    scanf("%f", &p.discount);
+
+    
+    p.dueAmount = p.totalAmount - p.discount;
 
     printf("\n--- Submitted Successfully! ---\n");
 
-
     printf("\n------ Patient Details ------\n");
-    printf("Name        : %s", p.name);
-    printf("ID          : %d\n", p.id);
-    printf("Address     : %s", p.address);
-    printf("Mobile No.  : %s", p.mobile);
-    printf("Doctor Name : %s", p.doctor);
+    printf("Name             : %s", p.name);
+    printf("ID               : %d\n", p.id);
+    printf("Age              : %d\n", p.age);
+    printf("Gender           : %s", p.gender);
+    printf("Address          : %s", p.address);
+    printf("Mobile No.       : %s", p.mobile);
+    printf("Doctor Name      : %s", p.doctor);
+    printf("Reference        : %s", p.reference);
+    printf("Total Amount     : %.2f\n", p.totalAmount);
+    printf("Discount         : %.2f\n", p.discount);
+    printf("Due Amount       : %.2f\n", p.dueAmount);
+
     return 0;
 }
